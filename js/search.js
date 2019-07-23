51de8234bc909525b546bc10aa96c730e85ee490
8ctopotamus <joshnaylor88@gmail.com>
2019-07-23 10:15:22 -0500

    Fix fileName split bug

--- js/search.js
+++ js/search.js
@@ -58,8 +58,9 @@
   }
 
   const renderResultHTML = obj => {
+    const fileName = obj.pdf ? obj.pdf.split('/').reverse()[0] : ''
     resultsList.innerHTML += `<li class="result-item">
-      <div><a href="${obj.pdf}" target="_blank">${obj.pdf.split('/').reverse()[0]}</a></div>
+      <div><a href="${obj.pdf}" target="_blank">${fileName}</a></div>
       <div>${obj.title}</div>
       <div>${obj.manual_type.join(', ')}<br/> ${obj.product_type.join(', ')}</div>
       <div>${obj.description}</div>
